41cd42bfa51ccd7d9220c00479e53b78c00de393
raj-subhankar <subhankar.rj@gmail.com>
2021-09-08 18:31:10 +0530

    Configure search API

---
+++ Response.js
@@ -0,0 +1,290 @@
+export default {
+  kind: "customsearch#search",
+  url: {
+    type: "application/json",
+    template:
+      "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json",
+  },
+  queries: {
+    request: [
+      {
+        title: "Google Custom Search - corona virus",
+        totalResults: "5",
+        searchTerms: "corona virus",
+        count: 5,
+        startIndex: 1,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "772cfc685eb66c1cc",
+      },
+    ],
+  },
+  context: {
+    title: "Google",
+  },
+  searchInformation: {
+    searchTime: 0.995539,
+    formattedSearchTime: "1.00",
+    totalResults: "5",
+    formattedTotalResults: "5",
+  },
+  spelling: {
+    correctedQuery: "coronavirus",
+    htmlCorrectedQuery: "<b><i>coronavirus</i></b>",
+  },
+  items: [
+    {
+      kind: "customsearch#result",
+      title: "Exposure Notifications: Helping fight COVID-19 - Google",
+      htmlTitle:
+        "Exposure Notifications: Helping fight <b>COVID</b>-19 - Google",
+      link: "https://www.google.com/covid19/exposurenotifications/",
+      displayLink: "www.google.com",
+      snippet:
+        "Learn how Exposure Notifications, built by Google and Apple, enable apps to send you a notification if you've likely been exposed to COVID-19.",
+      htmlSnippet:
+        "Learn how Exposure Notifications, built by Google and Apple, enable apps to send you a notification if you&#39;ve likely been exposed to <b>COVID</b>-19.",
+      cacheId: "qtK_I3x8mI4J",
+      formattedUrl: "https://www.google.com/covid19/exposurenotifications/",
+      htmlFormattedUrl:
+        "https://www.google.com/<b>covid19</b>/exposurenotifications/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQiwk52X8M4D7_y6Ep34qS_cb3XzaYv0NRD9jNw4Y1hPBwXlkqhv8pO4DAM",
+            width: "300",
+            height: "168",
+          },
+        ],
+        metatags: [
+          {
+            referrer: "no-referrer",
+            "og:image":
+              "https://lh3.googleusercontent.com/Sne21Iu5L03qnE4oTMtjmUWfOlM8zUwrfNXCAklun4wHuVi-u1Jd-7ukoXTCuiNnzl2KqTDy6gBizpMB7oU6BxHnQnFfGbh-OGoL=w1200",
+            "twitter:card": "summary_large_image",
+            "twitter:image:alt":
+              "Learn how Exposure Notifications, built by Google and Apple, enable apps to send you a notification if you’ve likely been exposed to COVID-19.",
+            "og:site_name":
+              "Exposure Notifications: Helping fight COVID-19 - Google",
+            viewport: "initial-scale=1, minimum-scale=1, width=device-width",
+            "og:title":
+              "Exposure Notifications: Helping fight COVID-19 - Google",
+            "og:url":
+              "https://www.google.com/intl/en_us/covid19/exposurenotifications/",
+            "og:description":
+              "Learn how Exposure Notifications, built by Google and Apple, enable apps to send you a notification if you’ve likely been exposed to COVID-19.",
+            "twitter:image":
+              "https://lh3.googleusercontent.com/Sne21Iu5L03qnE4oTMtjmUWfOlM8zUwrfNXCAklun4wHuVi-u1Jd-7ukoXTCuiNnzl2KqTDy6gBizpMB7oU6BxHnQnFfGbh-OGoL=w1200",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://lh3.googleusercontent.com/Sne21Iu5L03qnE4oTMtjmUWfOlM8zUwrfNXCAklun4wHuVi-u1Jd-7ukoXTCuiNnzl2KqTDy6gBizpMB7oU6BxHnQnFfGbh-OGoL=w1200",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "COVID-19 Community Mobility Reports",
+      htmlTitle: "<b>COVID</b>-19 Community Mobility Reports",
+      link: "https://www.google.com/covid19/mobility/",
+      displayLink: "www.google.com",
+      snippet:
+        "As global communities respond to COVID-19, we've heard from public health officials that the same type of aggregated, anonymized insights we use in products ...",
+      htmlSnippet:
+        "As global communities respond to <b>COVID</b>-19, we&#39;ve heard from public health officials that the same type of aggregated, anonymized insights we use in products&nbsp;...",
+      formattedUrl: "https://www.google.com/covid19/mobility/",
+      htmlFormattedUrl: "https://www.google.com/<b>covid19</b>/mobility/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpechhm3ksYMs5RhwpqSZFrTmTRHPhH8NH7JO707N9Lzwrc2Zf3PwHNs0a",
+            width: "310",
+            height: "163",
+          },
+        ],
+        metatags: [
+          {
+            "og:image":
+              "https://www.google.com/covid19/mobility/static/social_image.png",
+            "twitter:card": "summary_large_image",
+            "twitter:image:alt":
+              "See how your community is moving around differently due to COVID-19",
+            "og:site_name": "COVID-19 Community Mobility Report",
+            viewport: "width=device-width, initial-scale=1",
+            "og:title": "COVID-19 Community Mobility Report",
+            "og:url": "https://www.google.com/covid19/mobility?hl=en",
+            "og:description":
+              "See how your community is moving around differently due to COVID-19",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://www.google.com/covid19/mobility/static/social_image.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Thank You: Coronavirus helpers",
+      htmlTitle: "Thank You: <b>Coronavirus</b> helpers",
+      link: "https://www.google.com/doodles/thank-you-coronavirus-helpers",
+      displayLink: "www.google.com",
+      snippet:
+        "Thank You: Coronavirus helpers. April 18, 2020. As COVID-19 continues to · As COVID-19 continues to impact communities around the world, people are coming ...",
+      htmlSnippet:
+        "Thank You: <b>Coronavirus</b> helpers. April 18, 2020. As COVID-19 continues to &middot; As COVID-19 continues to impact communities around the world, people are coming&nbsp;...",
+      formattedUrl:
+        "https://www.google.com/doodles/thank-you-coronavirus-helpers",
+      htmlFormattedUrl:
+        "https://www.google.com/doodles/thank-you-<b>coronavirus</b>-helpers",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQCox-11vxJ7-QP37x-r0ia-c7FnPZHhh1eq6ToZoGJnwB-_c7HwE9zfu17",
+            width: "376",
+            height: "134",
+          },
+        ],
+        metatags: [
+          {
+            "og:image":
+              "http://www.google.com/logos/doodles/2020/thank-you-coronavirus-helpers-6753651837108764.2-2xa.gif",
+            "twitter:card": "summary_large_image",
+            "twitter:title": "Thank You: Coronavirus helpers",
+            "og:image:width": "1000",
+            "og:type": "video.other",
+            "og:title": "Thank You: Coronavirus helpers",
+            "og:image:height": "357",
+            "og:description":
+              "To all coronavirus helpers, thank you #GoogleDoodle #thankyouhelpers",
+            "twitter:image":
+              "http://www.google.com/logos/doodles/2020/thank-you-coronavirus-helpers-6753651837108764.2-2xa.gif",
+            "twitter:site": "@GoogleDoodles",
+            viewport: "width=device-width",
+            "twitter:description":
+              "To all coronavirus helpers, thank you #GoogleDoodle #thankyouhelpers",
+            "og:url":
+              "http://www.google.com/logos/doodles/2020/thank-you-coronavirus-helpers-6753651837108764.2-2xa.gif",
+          },
+        ],
+        cse_image: [
+          {
+            src: "http://www.google.com/logos/doodles/2020/thank-you-coronavirus-helpers-6753651837108764.2-2xa.gif",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Tips and Tutorials - Google for Nonprofits",
+      htmlTitle: "Tips and Tutorials - Google for Nonprofits",
+      link: "https://www.google.com/nonprofits/resources/coronavirus/",
+      displayLink: "www.google.com",
+      snippet:
+        "The Ad Grants team has put together this COVID-19 Resources guide to help you reach your community, find donors, and raise awareness for your cause.",
+      htmlSnippet:
+        "The Ad Grants team has put together this <b>COVID</b>-19 Resources guide to help you reach your community, find donors, and raise awareness for your cause.",
+      cacheId: "DoNALMNjOOIJ",
+      formattedUrl: "https://www.google.com/nonprofits/resources/coronavirus/",
+      htmlFormattedUrl:
+        "https://www.google.com/nonprofits/resources/<b>coronavirus</b>/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSA8A37CMDXCOxXhR5SCW5lQULT7O87GfxL41CdX1z3Va_vBIaQEmUBu-8",
+            width: "311",
+            height: "162",
+          },
+        ],
+        metatags: [
+          {
+            "og:image":
+              "https://lh3.googleusercontent.com/ltZJ370msKdnnmilBLJAQxkWA1rZvo4CTNn_IvaK04kd7S2TTj2aZw6TjwIfyaZCSpcXfFDSK08thqR49AqzG36nFUzeTh766pEW=w600-h314-rj",
+            "twitter:title":
+              "Tips & Tutorials for Nonprofits - Google for Nonprofits",
+            "twitter:site": "@googlenonprofit",
+            viewport: "initial-scale=1, minimum-scale=1, width=device-width",
+            "twitter:description":
+              "Resources to help your nonprofit navigate uncertainty. Explore tutorials, tips, and upcoming events to make the most of Google tools.",
+            "og:title":
+              "Tips & Tutorials for Nonprofits - Google for Nonprofits",
+            "og:description":
+              "Resources to help your nonprofit navigate uncertainty. Explore tutorials, tips, and upcoming events to make the most of Google tools.",
+            "twitter:image":
+              "https://lh3.googleusercontent.com/ltZJ370msKdnnmilBLJAQxkWA1rZvo4CTNn_IvaK04kd7S2TTj2aZw6TjwIfyaZCSpcXfFDSK08thqR49AqzG36nFUzeTh766pEW=w600-h314-rj",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://lh3.googleusercontent.com/ltZJ370msKdnnmilBLJAQxkWA1rZvo4CTNn_IvaK04kd7S2TTj2aZw6TjwIfyaZCSpcXfFDSK08thqR49AqzG36nFUzeTh766pEW=w600-h314-rj",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Coronavirus (COVID-19) - Google News",
+      htmlTitle: "<b>Coronavirus</b> (COVID-19) - Google News",
+      link: "https://www.google.com/covid19-map/search.php?kw=Variable",
+      displayLink: "www.google.com",
+      snippet:
+        '"Total doses given" shows the number of vaccine doses given to people. Since some vaccines require more than 1 dose, the number of fully vaccinated people ...',
+      htmlSnippet:
+        "&quot;Total doses given&quot; shows the number of vaccine doses given to people. Since some vaccines require more than 1 dose, the number of fully vaccinated people&nbsp;...",
+      formattedUrl: "https://www.google.com/covid19-map/search.php?kw=Variable",
+      htmlFormattedUrl:
+        "https://www.google.com/<b>covid19</b>-map/search.php?kw=Variable",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTlXoMDwgGEUJP5WQeTTQixzqQWzgdDM4d5_SPQ0iVxZ1Qa79VXF21pUX_u",
+            width: "275",
+            height: "183",
+          },
+        ],
+        metatags: [
+          {
+            "application-name": "News",
+            "og:image":
+              "https://lh3.googleusercontent.com/Sk9fLyVEoDk-5rONLR5ifRY3mmAmbeNSaM6mMLjLE-XeFLL1s4KXpzDFrRzD5Yl5xufCzbEyLow=w450-h300-n",
+            "theme-color": "white",
+            "og:type": "website",
+            "twitter:card": "summary",
+            "twitter:title": "Coronavirus (COVID-19) - Google News",
+            "og:image:width": "450",
+            "og:site_name": "Google News",
+            "apple-mobile-web-app-title": "News",
+            "og:title": "Coronavirus (COVID-19) - Google News",
+            "og:image:height": "300",
+            "og:image:type": "image/jpeg",
+            title: "Coronavirus (COVID-19) - Google News",
+            "og:description":
+              "See the map, stats, and news for areas affected by COVID-19 on Google News",
+            "twitter:image":
+              "https://lh3.googleusercontent.com/Sk9fLyVEoDk-5rONLR5ifRY3mmAmbeNSaM6mMLjLE-XeFLL1s4KXpzDFrRzD5Yl5xufCzbEyLow=w450-h300-n",
+            referrer: "origin",
+            "apple-mobile-web-app-status-bar-style": "black",
+            "msapplication-tap-highlight": "no",
+            viewport: "width=device-width,initial-scale=1,minimal-ui",
+            "apple-mobile-web-app-capable": "yes",
+            "twitter:description":
+              "See the map, stats, and news for areas affected by COVID-19 on Google News",
+            "mobile-web-app-capable": "yes",
+            "og:locale": "en-US",
+            "og:url":
+              "https://news.google.com/covid19/map?hl=en-US&gl=US&ceid=US:en",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://lh3.googleusercontent.com/Sk9fLyVEoDk-5rONLR5ifRY3mmAmbeNSaM6mMLjLE-XeFLL1s4KXpzDFrRzD5Yl5xufCzbEyLow=w450-h300-n",
+          },
+        ],
+      },
+    },
+  ],
+};
--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import { API_KEY, CONTEXT_KEY } from "../keys";
+import Response from "../Response";
 
-function Search() {
+function Search({ results }) {
+  console.log("results", results);
   return (
     <div>
       <Head>
@@ -14,3 +17,20 @@ function Search() {
 }
 
 export default Search;
+
+export async function getServerSideProps(context) {
+  const useDummyDate = false;
+  const startIndex = context.query.start || "0";
+
+  const data = useDummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+      ).then((response) => response.json());
+
+  return {
+    props: {
+      results: data,
+    },
+  };
+}
